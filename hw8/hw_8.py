7253d342a9a595c75cf2901ec75ced2c2112576d
eoershova <35444491+eoershova@users.noreply.github.com>
2018-04-27 01:19:09 +0300

    Add files via upload

---
+++ hw8/hw_8.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+
+def dictionary_maker():
+    d = defaultdict(list)
+    with open('words.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            sp = line.strip().split(';')
+            if len(sp) != 2:
+                continue
+            d[sp[0]].append(sp[1])
+    return d
+
+
+def riddles(d):
+    n = 0
+    w = 0
+    print('начало игры "угадайте существительное по словосочетанию"')
+    for i in d:
+        the_word = i
+        for x in range(0, len(d[i])):
+            clue = d[i][x]
+            print(clue, '.' * len(clue))
+            print('ваша догадка?')
+            guess = input()
+            if guess == the_word:
+                print('круто! вы угадали; переходим к следующему слову')
+                w += 1
+                break
+            else:
+                if x < len(d[i]) - 1:
+                    print('НЕТ. попробуйте угадать это же слово еще раз')
+                elif x == len(d[i]) - 1 and n + w != len(d) - 1:
+                    print('все еще нет, давайте попробуем другое слово')
+                    n += 1
+                elif n + w == len(d) - 1 and w >= 1:
+                    print('Поздравляю, игра окончена и вы даже что-то угадали')
+                else:
+                    print('НЕТ. Игра окончена.')
+
+
+def main():
+    d = dictionary_maker()
+    riddles(d)
+
+
+if __name__ == "__main__":
+    main()
