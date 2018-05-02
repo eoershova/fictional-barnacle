715bb00f6f59e39d6814fdc60597c2a60cdccddd
eoershova <35444491+eoershova@users.noreply.github.com>
2018-05-03 01:39:40 +0300

    Add files via upload

---
+++ hw9/hw_9.py
@@ -0,0 +1,42 @@
+import re
+
+
+def text_prep():
+    print('введите полное имя файла')
+    with open(input(), 'r', encoding='utf-8') as f:
+        text = f.read()
+        text = text.lower()
+        text = re.sub(r'[^а-я ]', ' ', text)
+    return text
+
+
+def forms_finder(text):
+    verb_infinitive = set(re.findall('выпить', text))
+    verb_present_forms = set(re.findall('выпива(?:ю|ешь|ем|ет|ют)', text))
+    verb_past_forms = set(re.findall('выпил[а-я]*', text))
+    verb_participle_active = set(re.findall('выпивш[а-р,т-я]{2,3}', text))
+    verb_participle_pass = set(re.findall('выпит[а-ы,э-я]{1,3}', text))
+    verb_participle_transgressive = set(re.findall('выпив\s|выпимши', text))
+    verb_reflexive_present_forms = set(re.findall('выпива(?:ю|ешь|ем|ет|ют)(?:ся|сь)', text))
+    verb_reflexive_past_forms = set(re.findall('выпил[а-я]*(?:ся|сь)', text))
+    verb_reflexive_participle_active = set(re.findall('выпивш[а-я]{2}(?:ся|сь)', text))
+# похоже, возвратных форм пассивного залога не существует
+# set() превращает список в беспорядочную коллекцию дистинктных обьетов, т е убирает повторения
+    print('инфинитив:', "\t".join(verb_infinitive))
+    print('формы глагола в настоящем времени:',"\t".join(verb_present_forms))
+    print('формы глагола в прошедшем времени:',"\t".join(verb_past_forms))
+    print('формы действительных причастий:',"\t".join(verb_participle_active))
+    print('формы страдательных причастий:',"\t".join(verb_participle_pass))
+    print('деепричатия:',"\t".join(verb_participle_transgressive))
+    print('возвратные формы глагола в настоящем времени:',"\t".join(verb_reflexive_present_forms))
+    print('возвратные формы глагола в прошедшем времени:', "\t".join(verb_reflexive_past_forms))
+    print('возвратные формы действительных причастий:', "\t".join(verb_reflexive_participle_active))
+
+
+def main():
+    text = text_prep()
+    forms_finder(text)
+
+
+if __name__ == "__main__":
+    main()
