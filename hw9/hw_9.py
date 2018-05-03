fd1e7c0376eff0371f04c97d8214619ae63ad6c8
eoershova <35444491+eoershova@users.noreply.github.com>
2018-05-03 10:24:03 +0300

    Add files via upload

--- hw9/hw_9.py
+++ hw9/hw_9.py
@@ -12,25 +12,23 @@ def text_prep():
 
 def forms_finder(text):
     verb_infinitive = set(re.findall('выпить', text))
-    verb_present_forms = set(re.findall('выпива(?:ю|ешь|ем|ет|ют)', text))
+    verb_future_forms = set(re.findall('выпь(?:ю|ешь|ем|ет|ют)', text))
     verb_past_forms = set(re.findall('выпил[а-я]*', text))
     verb_participle_active = set(re.findall('выпивш[а-р,т-я]{2,3}', text))
-    verb_participle_pass = set(re.findall('выпит[а-ы,э-я]{1,3}', text))
+    verb_participle_pass = set(re.findall('выпит[а-ы,э-я]{0,3}', text))
     verb_participle_transgressive = set(re.findall('выпив\s|выпимши', text))
-    verb_reflexive_present_forms = set(re.findall('выпива(?:ю|ешь|ем|ет|ют)(?:ся|сь)', text))
     verb_reflexive_past_forms = set(re.findall('выпил[а-я]*(?:ся|сь)', text))
-    verb_reflexive_participle_active = set(re.findall('выпивш[а-я]{2}(?:ся|сь)', text))
-# похоже, возвратных форм пассивного залога не существует
+    verb_reflexive_participle_active = set(re.findall('выпивш[а-р,т-я]{2,3}(?:ся|сь)', text))
+# похоже, форм настощего времени и возвратных форм пассивного залога не существует
 # set() превращает список в беспорядочную коллекцию дистинктных обьетов, т е убирает повторения
     print('инфинитив:', "\t".join(verb_infinitive))
-    print('формы глагола в настоящем времени:',"\t".join(verb_present_forms))
+    print('формы глагола в будущем времени:',"\t".join(verb_future_forms))
     print('формы глагола в прошедшем времени:',"\t".join(verb_past_forms))
     print('формы действительных причастий:',"\t".join(verb_participle_active))
     print('формы страдательных причастий:',"\t".join(verb_participle_pass))
     print('деепричатия:',"\t".join(verb_participle_transgressive))
-    print('возвратные формы глагола в настоящем времени:',"\t".join(verb_reflexive_present_forms))
-    print('возвратные формы глагола в прошедшем времени:', "\t".join(verb_reflexive_past_forms))
-    print('возвратные формы действительных причастий:', "\t".join(verb_reflexive_participle_active))
+    print('возвратные формы глагола в прошедшем времени:',"\t".join(verb_reflexive_past_forms))
+    print('возвратные формы действительных причастий:',"\t".join(verb_reflexive_participle_active))
 
 
 def main():
@@ -40,3 +38,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#test2.txt
