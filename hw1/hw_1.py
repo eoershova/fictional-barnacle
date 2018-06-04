04aa6810db60d9574c9fcd1e6b4f00d6d5dac1c9
eoershova <35444491+eoershova@users.noreply.github.com>
2018-06-04 09:15:55 +0300

    Update hw_1.py

--- hw1/hw_1.py
+++ hw1/hw_1.py
@@ -8,7 +8,7 @@ if a + b == c:
     print('a и b дают в сумме с')
 else:
     print('a и b не дают в сумме с')
-if a // b == c:
+if b != 0 and a // b == c:
     print('a разделить на b равно c')
 else:
     print('a разделить на b не равно c')
