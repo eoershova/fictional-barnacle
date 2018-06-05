cc29d78d8630719f4f39dded5ca42aa25ac14bbe
eoershova <35444491+eoershova@users.noreply.github.com>
2018-06-05 16:50:39 +0300

    Update hw13.py

--- hw13/hw13.py
+++ hw13/hw13.py
@@ -6,13 +6,15 @@ import re
 def folder_finder():
     start_path = '.'
     letter = collections.Counter()
-    not_letter = r'[^A-Za-z]'
+    not_letter = r'[^A-Za-zА-Яа-я]' #учитываю только латиницу и кириллицу
     for root, dirs, files in os.walk(start_path):
         for directory in dirs:
+            directory = directory.lower() #потому что некоторые названия начинаются с большой буквы, а некоторые - нет
             if re.search(not_letter, directory[0]):
                 continue
             else:
                 letter.update(directory[0])
+    print(letter)
     return letter
 
 
